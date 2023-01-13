833d705cf7d0d22b73ec35184e042ce7cc59551e
padigar210 <shriharipadigar@gmail.com>
2023-01-13 23:02:09 +0530

    mistakes resolved

--- client/script.js
+++ client/script.js
@@ -212,9 +212,10 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId)
 
     // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+    loader(messageDiv);
 
     //fetch data from the server -> bot's response
+
     // const response = await fetch('http://localhost:5000/', {
     const response = await fetch('https://codex-i0ts.onrender.com/', {
         method: 'POST',
